329172f3e551e8536a7b310fe7aa602d62d51d66
kkgoyal263 <Ryomensukuna24k@gmail.com>
2026-03-01 19:26:46 +0530

    Building Server

---
+++ server.js
@@ -0,0 +1,12 @@
+// console.log('We have a package.json!')
+import http from 'node:http'
+
+const PORT = 8000
+
+const server = http.createServer((req, res) => {
+    res.end('Hello from the server!')
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
