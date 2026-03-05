07d04a1c2630745ca90433c8ee3294508447e6ff
kkgoyal263 <Ryomensukuna24k@gmail.com>
2026-03-05 23:45:21 +0530

    database is added

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 // console.log('We have a package.json!')
 import http from 'node:http'
+import {getDataFromDB} from '/database/db.js'
 
 const PORT = 8000
 
-const server = http.createServer((req, res) => {
-    res.end('Hello from the server!')
+const server = http.createServer(async(req, res) => {
+
+    const destinations = await getDataFromDB()
+    if(req.url === '/api' && req.method === 'GET'){
+
+        res.setHeader('Content-Type', 'application/json')
+        res.statusCode = 200
+        res.end(JSON.stringify(destinations))
+    } 
+    else if(req.url.startsWith('/api/continent') && req.method === 'GET'){
+        const continent = req.url.split('/').pop()
+        console.log(continent)
+    }
+    else {
+        res.setHeader('Content-Type', 'application/jso')
+        res.statusCode = 404
+        res.end(JSON.stringify({error:'not found', message: 'Route not found'}))
+    }
 })
 
 server.listen(PORT, () => {
